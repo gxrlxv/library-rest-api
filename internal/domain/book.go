454abd6a23c01322c5574e5699b73f80fb079544
gxrlxv <gorlov1207@gmail.com>
2022-06-03 14:33:40 +0300

    update book struct

--- internal/domain/book.go
+++ internal/domain/book.go
@@ -1,7 +1,23 @@
 package domain
 
 type Book struct {
-	ID     string `json:"id,omitempty"`
+	ID     string `json:"id" bson:"_id,omitempty"`
+	Name   string `json:"name" bson:"name"`
+	Year   int    `json:"year" bson:"year"`
+	Author Author `json:"author" bson:"author"`
+	Genre  string `json:"genre" bson:"genre"`
+	Busy   bool   `json:"busy" bson:"busy"`
+	Owner  User   `json:"owner" bson:"owner"`
+}
+
+type CreateBookDTO struct {
+	Name   string `json:"name"`
+	Year   int    `json:"year"`
+	Author Author `json:"author"`
+	Genre  string `json:"genre"`
+}
+
+type UpdateBookDTO struct {
 	Name   string `json:"name,omitempty"`
 	Year   int    `json:"year,omitempty"`
 	Author Author `json:"author,omitempty"`
